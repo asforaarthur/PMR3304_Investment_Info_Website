0ac4f19cc36f388d6214e5d57b15d68cad0504be
Arthur Alves dos Santos Asfora <asforaarthur@usp.br>
2023-11-16 23:27:20 -0300

    Adicionar modificações para implantação no Render

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
 cat <<EOF | python manage.py shell
 import os
 from django.contrib.auth import get_user_model

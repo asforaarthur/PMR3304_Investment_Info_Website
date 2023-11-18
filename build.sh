17d9085759fddb1a545e98816395ea01424e8e10
Arthur Alves dos Santos Asfora <asforaarthur@usp.br>
2023-11-18 12:44:10 -0300

    Adaptar configurações do banco de dados para implantação no Render - Final [6]

--- build.sh
+++ build.sh
@@ -18,7 +18,6 @@ User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() o
     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
 
 g = Group(name="Basic users")
-g.save()
 g.permissions.set([Permission.objects.get(codename=c) for c in ["add_user","view_user","add_list","view_list", "add_comentario", "view_investment"]])
 Group.objects.filter(name="Basic users").exists() or g.save()
 EOF

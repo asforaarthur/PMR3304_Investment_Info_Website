e4240a106ea72bdb3d672ad180674dc7a85462d2
Arthur Alves dos Santos Asfora <asforaarthur@usp.br>
2023-11-18 12:41:45 -0300

    Adaptar configurações do banco de dados para implantação no Render - Final [5]

--- build.sh
+++ build.sh
@@ -1,37 +1,24 @@
-#!/bin/bash
+
 set -o errexit
 
-# Install Python dependencies
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
 
-# Run database migrations
 python manage.py migrate
 
-# Ensure Django settings are configured
-export DJANGO_SETTINGS_MODULE=investmentsite.settings
-
-# Create superuser if not existing
-DJANGO_SUPERUSER_USERNAME="admin"
-DJANGO_SUPERUSER_EMAIL="asforaarthur@usp.br"
-DJANGO_SUPERUSER_PASSWORD="123456"
-
-python -c "import os; from django.contrib.auth import get_user_model; from django.conf import settings; settings.configure(); User = get_user_model(); User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists() or User.objects.create_superuser(os.environ['DJANGO_SUPERUSER_USERNAME'], os.environ['DJANGO_SUPERUSER_EMAIL'], os.environ['DJANGO_SUPERUSER_PASSWORD'])"
-
-# Create or retrieve Group "Basic users"
-python manage.py shell <<EOF
+cat <<EOF | python manage.py shell
+import os
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
-from django.conf import settings
 
-settings.configure()
+User = get_user_model()
 
-# Attempt to retrieve the group or create it if it doesn't exist
-group, created = Group.objects.get_or_create(name="Basic users")
+User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
 
-# If the group was created, assign permissions
-if created:
-    permissions = Permission.objects.filter(codename__in=["add_user", "view_user", "add_list", "view_list", "add_comentario", "view_investment"])
-    group.permissions.add(*permissions)
+g = Group(name="Basic users")
+g.save()
+g.permissions.set([Permission.objects.get(codename=c) for c in ["add_user","view_user","add_list","view_list", "add_comentario", "view_investment"]])
+Group.objects.filter(name="Basic users").exists() or g.save()
 EOF

90e08dea3c7ad83a9f2bd461e018fd4e352ba747
Arthur Alves dos Santos Asfora <asforaarthur@usp.br>
2023-11-18 12:32:57 -0300

    Adaptar configurações do banco de dados para implantação no Render - Final [2]

--- build.sh
+++ build.sh
@@ -1,23 +1,31 @@
-
+#!/bin/bash
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Run database migrations
 python manage.py migrate
 
-cat <<EOF | python manage.py shell
-import os
-from django.contrib.auth import get_user_model
+# Create superuser if not existing
+DJANGO_SUPERUSER_USERNAME="your_superuser_username"
+DJANGO_SUPERUSER_EMAIL="your_superuser_email@example.com"
+DJANGO_SUPERUSER_PASSWORD="your_superuser_password"
+
+python -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists() or User.objects.create_superuser(os.environ['DJANGO_SUPERUSER_USERNAME'], os.environ['DJANGO_SUPERUSER_EMAIL'], os.environ['DJANGO_SUPERUSER_PASSWORD'])"
+
+# Create or retrieve Group "Basic users"
+python manage.py shell <<EOF
 from django.contrib.auth.models import Group, Permission
 
-User = get_user_model()
+# Attempt to retrieve the group or create it if it doesn't exist
+group, created = Group.objects.get_or_create(name="Basic users")
 
-User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
-    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
-
-g = Group(name="Basic users")
-g.permissions.set([Permission.objects.get(codename=c) for c in ["add_user","view_user","add_list","view_list", "add_comentario", "view_investment"]])
-Group.objects.filter(name="Basic users").exists() or g.save()
+# If the group was created, assign permissions
+if created:
+    permissions = Permission.objects.filter(codename__in=["add_user", "view_user", "add_list", "view_list", "add_comentario", "view_investment"])
+    group.permissions.add(*permissions)
 EOF
